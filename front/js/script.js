163ea7a3c26041b3d13eef3181173bf696521f1f
raed ABBAS <raed.abbas@hotmail.fr>
2022-07-18 16:27:03 +0200

    creation de repo, js, script

---
+++ front/js/script.js
@@ -0,0 +1,21 @@
+fetch("http://localhost:3000/api/products")
+    .then((res) => {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then(function (value) {
+        console.log(value);
+        value.forEach(element => {
+            console.log(element);
+            document.getElementById("items").innerHTML += `<a href="./product.html?id="${element._id}">
+        <article>
+        <img src="${element.imageUrl}" alt="${element.altTxt}">
+        <h3 class="productName">${element.name}</h3>
+        <p class="productDescription">${element.description}</article>
+    </a>`
+        });
+    })
+    .catch(function (err) {
+
+    });
